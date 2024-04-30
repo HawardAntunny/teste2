68b0bfb1dc458862c00b94e4934a93d5455ee61d
Haward Antunny <127453907+HawardAntunny@users.noreply.github.com>
2024-04-30 11:34:34 -0300

    ggg

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for(let i=0;i <botoes.length;i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date ("2024-03-11T00:00:00");
+const tempoObjetivo1 = new Date ("2024-06-11T00:00:00");
 const tempoObjetivo2 = new Date ("2024-04-01T00:00:00");
 const tempoObjetivo3 = new Date ("2024-05-29T00:00:00");
 const tempoObjetivo4 = new Date ("2024-08-01T00:00:00");
